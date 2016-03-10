750ddaba9346fff90edc819eeebdb12652fcbc7b
Eric Roberts <eric@threedot14.com>
2016-03-10 15:46:51 +0000

    Add Comments to sample

--- stories/sample.js
+++ stories/sample.js
@@ -6,6 +6,12 @@
  */
 
 (function () {
+    /*****************
+     * Game instance *
+     *****************/
+     
+    // This is the instance of SampleGame that represents our story. When the user
+    // restarts the game we will create a new instance of SampleGame and put it here.
     var cGame = null;
     
     var getMap =
@@ -13,6 +19,22 @@
 <text x="5" y="5" font-size="80">${i}</text>
 `;
     
+    /**********
+     * Locale *
+     **********/
+     
+    // The locale object contains all the text that will be printed. The object
+    // is called "locale" because it can easily be replaced with other locales for
+    // different languages. For instance, we could consider this locale "en_US"
+    // and create another locale "es" for spanish. Ideally, each locale could be
+    // stored in seperate files.
+    // Make sure this variable only contains strings, or things that could easily
+    // represented as strings. Although it may be tempting to do something like this:
+    //    var locale = { count: (i) => `You have ${i} items!` }
+    // The function, count, cannot be easily stored as a string. If you want to do
+    // substitution, you're better off doing
+    //    var locale = { count: "You have %items% items!" }
+    // and replacing items.
     var locale = {
         cantproc: "Unable to process command!",
         welc: `
@@ -31,7 +53,7 @@ To continue to the next section please answer the following riddle. To answer, t
 
 > What walks on four legs in the morning, two legs in the afternoon, and three legs in the evening?
 `,
-    ridans: /(person)|(human)/,
+    ridans: /(person)|(human)/i,
     ridcorrect: `
 You correctly answered the riddle! See, you're getting the hang of this! For the next challenge
 I'm thinking of a number between 1 and 10. After you guess, I'll tell you if you're guess was higher or
@@ -48,6 +70,7 @@ You have completed the sample story for mhsgame. It's now time for you to pave y
 and add it to index.html scripts. Remember to use the sample.js file because others might not be in the correct format. Good luck
 and have fun!
 `,
+    // It's Okay to nest objects and arrays within locale.
     gameend: [
         "The game is over.",
         "I said the game is over",
@@ -56,40 +79,62 @@ and have fun!
         "-",
         "-",
         "Seriosuly, go home."
-    ]
-    };
+    ],
+};
 
+    /********************
+     * Helper Functions *
+     ********************/
+     
+    // When the user hits ENTER after typing a command your story will be passed
+    // the raw value of the textbox, including spaces. This simply processes the
+    // command into a more processable form.
     function procCmd(cmd) {
         return cmd.trim().split(" ");
     }
 
+    /**************
+     * SampleGame *
+     **************/
+    
+    // A SampleGame instance is created when the user starts the game. ALL game
+    // state should be stored in your game's class. DO NOT use cookies, localStorage
+    // global variable, or other state that is persistent between games.
     function SampleGame(game) {
         this.game = game;
         this.section = 0;
         
+        // Attach methods passed by game to the SampleGame object for convienence.
+        // This could also be done with prototypes.
         this.tell = game.tell.bind(null);
         this.sanitize = game.sanitize.bind(null);
         this.setMap = game.map.bind(null);
         
+        // The story is split into sections for easier organization. Variables
+        // specific for each section are put here.
         // SEC_GUESS
         this.guess_ans = 9;
         this.guess_tries = 1;
-        
         // SEC_END
         this.end_reply = 0;
     }
     SampleGame.prototype.respond = function (command) {
         var cmd = procCmd(command);
         
+        // Before we do anything make sure the command is valid.
         if ( cmd.length < 1 ) {
+            // Note how we only call tell with strings defined in locale
             this.tell(locale.cantproc);
             return;
         }
         
+        // Do section-specific actions
+        
         if ( this.section == this.SEC_START ) {
             this.tell(locale.welc);
             this.section = this.SEC_RIDDLE;
-            return;
+            return; // If we don't return the if will fall through and another
+                    // section will execute before we're ready.
         }
         
         if ( this.section == this.SEC_RIDDLE ) {
@@ -139,6 +184,7 @@ and have fun!
             }
         }
     }
+    // Each section has a number assosiated with it.
     SampleGame.prototype.SEC_START  = 0;
     SampleGame.prototype.SEC_RIDDLE = 1;
     SampleGame.prototype.SEC_GUESS  = 2;
@@ -146,7 +192,7 @@ and have fun!
     
     /* global mhsgame */
     mhsgame.registerStory({
-        name: "sample",
+        name: "sample", // Name should be short and lowercase
         description: "A template for creating your own games",
     }, function (command, game) {
         if ( command == "_start" ) {
