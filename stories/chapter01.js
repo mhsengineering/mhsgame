aecbca615c0a090653659d03ca9ea98ea931e9f1
Wliu <Wliu1402@gmail.com>
2016-04-03 22:17:30 -0400

    Rough outline of the chapter

--- stories/chapter01.js
+++ stories/chapter01.js
@@ -9,16 +9,32 @@
      * Game instance *
      *****************/
     var cGame = null;
-    
-    var getMap =
-(i) => `
-<text x="5" y="5" font-size="80">${i}</text>
-`;
-    
+
+    var getMap = (i) => `
+    <path d="` +
+      /* Cafeteria */
+      `M 50 100 L 0 100 L 0 0 L 80 0 L 80 20`+
+      /* Atrium */
+      `M 100 120 L 50 120 L 50 80 M 50 70 L 50 20
+      L 170 20 L 170 50 M 170 60 L 170 120 L 135 120`+
+      /* Classroom */
+      `M 160 20 L 160 0 L 240 0 L 240 100 L 170 100
+    " stroke="#000" stroke-width="3" fill="none" />
+
+    <text x="18" y="80" font-size="20" transform="rotate(270 18 80)">
+    Cafeteria
+    </text>
+    <text x="70" y="70" font-size="20">
+    Atrium
+    </text>
+    <text x="190" y="10" font-size="20" transform="rotate(90 190 10)">
+    Classroom
+    </text>`;
+
     /**********
      * Locale *
      **********/
-     
+
     // The locale object contains all the text that will be printed. The object
     // is called "locale" because it can easily be replaced with other locales for
     // different languages. For instance, we could consider this locale "en_US"
@@ -32,56 +48,41 @@
     //    var locale = { count: "You have %items% items!" }
     // and replacing items.
     var locale = {
-        cantproc: "Unable to process command!",
-        welc: `
-# Chapter 1
+        unknown: "Unable to process command!",
+        start: `# Chapter 1
 
-Welcome to the sample mhsgame! This story acts as a sample for people who want to write
-stories for mhsgame in the future. This is formatted as [Markdown](https://en.wikipedia.org/wiki/Markdown)
-and stored with the code to the game. Markdown supports ruch editing features such as **bold**, *italics*,
-[links](https://en.wikipedia.org/wiki/Lead-cooled_fast_reactor), and even images!
+                It's the first day of school, and you've just been dropped off outside the main atrium.`,
+        principal: `Mr. Manning, the principal, is busy greeting everyone and telling them to go to the
+                    auditorium for orientation.`,
+        greet: `>Why hello there!  Welome to Morristown High School!  What's your name?`,
+        reaponse: `Nice to meet you, %name%.  I hope you enjoy MHS.  Now why don't you head to the auditorium
+                   for orientation?`,
+        guessrange: "The number is between 1 and 10. Try again.",
+        guesshigh: "You guessed high!",
+        guesslow: "You guessed low!",
+        guesscorrect: `
+    You guessed the number correctly! It only took you %tries% tries!
 
-All these rich text features are important because mhsgame is a [text adventure game](https://en.wikipedia.org/wiki/Interactive_fiction)
-(AKA interactive fiction). Unlike more modern video games which may tell stories through movie-esque moving pictures and quick
-interaction, text adventure games rely on the power of language to convey messages.
-
-To continue to the next section please answer the following riddle. To answer, type in your answer in the text box below and hit enter.
-
-> What walks on four legs in the morning, two legs in the afternoon, and three legs in the evening?
-`,
-    ridans: /(person)|(human)/i,
-    ridcorrect: `
-You correctly answered the riddle! See, you're getting the hang of this! For the next challenge
-I'm thinking of a number between 1 and 10. After you guess, I'll tell you if you're guess was higher or
-lower than the correct answer.
-`,
-    ridwrong: "You didn't answer the riddle correctly. Try again and don't cheat!",
-    guessrange: "The number is between 1 and 10. Try again.",
-    guesshigh: "You guessed high!",
-    guesslow: "You guessed low!",
-    guesscorrect: `
-You guessed the number correctly! It only took you %tries% tries!
-
-You have completed the sample story for mhsgame. It's now time for you to pave your own adventure. Copy sample.js to a new file
-and add it to index.html scripts. Remember to use the sample.js file because others might not be in the correct format. Good luck
-and have fun!
-`,
-    // It's Okay to nest objects and arrays within locale.
-    gameend: [
-        "The game is over.",
-        "I said the game is over",
-        "Remember how I said the game is over earlier? Go home.",
-        "I'm ignoring you.",
-        "-",
-        "-",
-        "Seriosuly, go home."
-    ],
-};
+    You have completed the sample story for mhsgame. It's now time for you to pave your own adventure. Copy sample.js to a new file
+    and add it to index.html scripts. Remember to use the sample.js file because others might not be in the correct format. Good luck
+    and have fun!
+    `,
+        // It's Okay to nest objects and arrays within locale.
+        gameend: [
+            "The game is over.",
+            "I said the game is over",
+            "Remember how I said the game is over earlier? Go home.",
+            "I'm ignoring you.",
+            "-",
+            "-",
+            "Seriosuly, go home."
+        ],
+    };
 
     /********************
      * Helper Functions *
      ********************/
-     
+
     // When the user hits ENTER after typing a command your story will be passed
     // the raw value of the textbox, including spaces. This simply processes the
     // command into a more processable form.
@@ -90,22 +91,22 @@ and have fun!
     }
 
     /**************
-     * SampleGame *
+     * Chapter01 *
      **************/
-    
-    // A SampleGame instance is created when the user starts the game. ALL game
+
+    // A Chapter01 instance is created when the user starts the game. ALL game
     // state should be stored in your game's class. DO NOT use cookies, localStorage
     // global variable, or other state that is persistent between games.
-    function SampleGame(game) {
+    function Chapter01(game) {
         this.game = game;
         this.section = 0;
-        
-        // Attach methods passed by game to the SampleGame object for convienence.
+
+        // Attach methods passed by game to the Chapter01 object for convienence.
         // This could also be done with prototypes.
         this.tell = game.tell.bind(null);
         this.sanitize = game.sanitize.bind(null);
         this.setMap = game.map.bind(null);
-        
+
         // The story is split into sections for easier organization. Variables
         // specific for each section are put here.
         // SEC_GUESS
@@ -114,25 +115,25 @@ and have fun!
         // SEC_END
         this.end_reply = 0;
     }
-    SampleGame.prototype.respond = function (command) {
+    Chapter01.prototype.respond = function (command) {
         var cmd = procCmd(command);
-        
+
         // Before we do anything make sure the command is valid.
         if ( cmd.length < 1 ) {
             // Note how we only call tell with strings defined in locale
             this.tell(locale.cantproc);
             return;
         }
-        
+
         // Do section-specific actions
-        
+
         if ( this.section == this.SEC_START ) {
             this.tell(locale.welc);
             this.section = this.SEC_RIDDLE;
             return; // If we don't return the if will fall through and another
                     // section will execute before we're ready.
         }
-        
+
         if ( this.section == this.SEC_RIDDLE ) {
             if ( locale.ridans.test(cmd[0]) ) {
                 this.tell(locale.ridcorrect);
@@ -142,14 +143,14 @@ and have fun!
             }
             return;
         }
-        
+
         if ( this.section == this.SEC_GUESS ) {
             var g = parseInt(cmd[0],10);
             if (isNaN(g)) {
                 this.tell(locale.cantproc);
                 return;
             }
-            
+
             if ( g < 0 || g > 10 ) {
                 this.tell(locale.guessrange);
                 return;
@@ -164,15 +165,15 @@ and have fun!
                 this.guess_tries++;
                 return;
             }
-            
+
             this.tell(
                 locale.guesscorrect.replace("%tries%",this.guess_tries.toString(10))
                 );
             this.section = this.SEC_END;
-            
+
             return;
         }
-        
+
         if ( this.section == this.SEC_END ) {
             this.tell(locale.gameend[this.end_reply]);
             if ( this.end_reply < locale.gameend.length-1 ) {
@@ -181,19 +182,19 @@ and have fun!
         }
     }
     // Each section has a number assosiated with it.
-    SampleGame.prototype.SEC_START  = 0;
-    SampleGame.prototype.SEC_RIDDLE = 1;
-    SampleGame.prototype.SEC_GUESS  = 2;
-    SampleGame.prototype.SEC_END    = 3;
-    
+    Chapter01.prototype.SEC_START  = 0;
+    Chapter01.prototype.SEC_RIDDLE = 1;
+    Chapter01.prototype.SEC_GUESS  = 2;
+    Chapter01.prototype.SEC_END    = 3;
+
     /* global mhsgame */
     mhsgame.registerStory({
-        name: "sample", // Name should be short and lowercase
+        name: "chapter01", // Name should be short and lowercase
         description: "A template for creating your own games",
     }, function (command, game) {
         if ( command == "_start" ) {
             // Reset Game
-            cGame = new SampleGame(game);
+            cGame = new Chapter01(game);
         }
         cGame.respond(command);
     });
